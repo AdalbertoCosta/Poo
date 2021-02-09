cfca6e126584e7d7c64351d267c9b7c78d18c455
Juninho <adalberto.caj@gmail.com>
2021-02-09 11:10:26 -0300

    primeiros passos em java

---
+++ Exemplos/prjExemplo2/src/Exemplo2.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Adalberto Costa
+ */
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int a=5, b=10;
+        
+        a = a + 5;
+        b = b * 2;
+        
+        System.out.println("Valor da variável a: " + a);
+        System.out.println("Valor da variável b: " + b);
+    }
+    
+}
